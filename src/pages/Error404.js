4c5e683f97d68561c3f173e8b60bca6ba939fad5
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-10-21 12:42:12 +0200

    error page styled. added media queries. step 6 finished.

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,11 +1,15 @@
 // src/pages/Error404.js
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/" className="error__link">
+        Retourner sur la page d'accueil
+      </NavLink>
     </div>
   );
 };
