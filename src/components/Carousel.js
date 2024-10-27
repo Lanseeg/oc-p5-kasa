6ad2c04fe6fca1578fcffc5ebcf2f347601b61f3
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-10-28 00:30:00 +0100

    fix. carousel logic & style. max width set to prevent to much stretching. Host style fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import '../styles/components/_carousel.scss';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import "../styles/components/_carousel.scss";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0); // Current image index
   const totalImages = images.length;
 
-  // Right clic handling
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages); // Loop after last image
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
   };
 
-  // Left click handling
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? totalImages - 1 : prevIndex - 1
@@ -21,11 +22,11 @@ const Carousel = ({ images }) => {
 
   return (
     <div className="carousel">
-      {/* Image container with translation */}
       <div
         className="carousel__track"
         style={{
-          transform: `translateX(-${currentIndex * 100}%)`, // Horizontal scrolling
+          transform: `translateX(-${currentIndex * (100 / totalImages)}%)`,
+          width: `${100 * totalImages}%`, // Track width covers all images.
         }}
       >
         {images.map((image, index) => (
@@ -34,23 +35,22 @@ const Carousel = ({ images }) => {
             src={image}
             alt={`Slide ${index + 1}`}
             className="carousel__image"
+            style={{ width: `${100 / totalImages}%` }} // Each image occupies its portion of the track.
           />
         ))}
       </div>
 
-      {/* Display arrows only if several images */}
       {totalImages > 1 && (
         <>
           <button className="carousel__arrow left" onClick={prevSlide}>
-            <FontAwesomeIcon icon={faChevronLeft} /> {/* Left arrow */}
+            <FontAwesomeIcon icon={faChevronLeft} />
           </button>
           <button className="carousel__arrow right" onClick={nextSlide}>
-            <FontAwesomeIcon icon={faChevronRight} /> {/* Right arrow */}
+            <FontAwesomeIcon icon={faChevronRight} />
           </button>
         </>
       )}
 
-      {/* Display index only if several images */}
       {totalImages > 1 && (
         <div className="carousel__counter">
           {currentIndex + 1}/{totalImages}
